943b41f6401b242aebc62c7dc85af6178e2e3ffc
Julian Loh <julianloh@Julians-iMac.local>
2026-03-29 14:48:26 +0800

    debug live env

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -39,6 +39,9 @@ export default function LoginPage() {
     };
   }, []);
 
+  console.log("LIVE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("LIVE KEY:", process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
+
   async function handlePasswordLogin(e: React.FormEvent) {
     e.preventDefault();
 
